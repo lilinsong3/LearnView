d4351331b96c06a70fabd169aa79c7436b8a186a
lilinsong3 <imlilinsong@163.com>
2023-10-17 20:36:00 +0800

    add DazzlingBoard demo

---
+++ app/src/main/java/com/github/lilinsong3/learnview/ui/dazzlingboard/DazzlingBoardFragment.kt
@@ -0,0 +1,41 @@
+package com.github.lilinsong3.learnview.ui.dazzlingboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.github.lilinsong3.learnview.databinding.FragmentDazzlingBoardBinding
+
+/**
+ * An example full-screen fragment that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+class DazzlingBoardFragment : Fragment() {
+
+    private var _binding: FragmentDazzlingBoardBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentDazzlingBoardBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
